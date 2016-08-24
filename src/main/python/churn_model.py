a64acae93fcc1e004f7a01c5251bf8306c9c531a
Bilind Hajer <bilind@brightwhistle.com>
2016-08-24 14:40:25 -0400

    spark decision tree model complete

--- src/main/python/churn_model.py
+++ src/main/python/churn_model.py
@@ -3,21 +3,16 @@ from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.tree import DecisionTree
 from pyspark.sql import SQLContext, DataFrame, Column, Row
 from pyspark.sql.types import *
-
 from pyspark.sql.functions import UserDefinedFunction
-
 import time
 
-# KS,128,415,No,Yes,25,265.1,110,45.07,197.4,99,16.78,244.7,91,11.01,10.0,3,2.7,1,False
+def getRegionFromState(state): 
+	region, _  = filter( lambda (region,stateSet): state.upper() in stateSet, stateMap.items() )[0]
+	return region
 
-binaryMap = { 'Yes' : 1.0, 'No' : 0.0, 'True' : 1.0, 'False' : 0.0  }
-
-stateSet1 = ['WA', 'WI', 'FL', 'WY', 'NH', 'NJ', 'TX', 'ND', 'TN', 'CA', 'NV', 'CO', 'AK', 'VT', 'GA', 'IA', 'MA', 'ME', 'OK', 'MO', 'MI', 'KS', 'MS', 'SC']
-stateSet2 = ['KY', 'DE', 'DC', 'WV', 'HI', 'NM', 'LA', 'NC', 'NE', 'NY', 'PA', 'RI', 'VA', 'AL', 'AR', 'IL', 'IN', 'AZ','ID','CT','MD','OH','UT','MN','MT','OR', 'SD']
-
-stateMap = { 1 : stateSet1, 2: stateSet2 }
-toState = UserDefinedFunction( lambda state: filter( lambda (region,stateSet): state.upper() in stateSet, stateMap ).keys()[0], IntegerType() )
-toNum = UserDefinedFunction( lambda k: binaryMap[k], DoubleType() )
+def labelData(data): 
+	data =  data.rdd.map( lambda row: LabeledPoint( row[0], row[1:] ) )
+	return data
 
 def f(x):
 	print x
@@ -26,24 +21,24 @@ def makeSqlRow(line):
  	line = line.replace(" ","").split(",")
 
         state = line[0]
-        acctLength = int( line[1] )
+        acctLength = float( line[1] )
         areaCode = line[2]
         internationalPlan = line[3]
         voiceMailPlan = line[4]
-        numOfVmailMessages = int( line[5] )
+        numOfVmailMessages = float( line[5] )
         totalDayMinutes = float( line[6])
-        totalDayCalls = int( line[7] )
+        totalDayCalls = float( line[7] )
         totalDayCharge = float( line[8] )
         totalEveningMinutes = float( line[9] )
-        totalEveningCalls = int( line[10] )
+        totalEveningCalls = float( line[10] )
         totalEveningCharge = float( line[11] )
         totalNightMinutes = float(line[12] )
-        totalNightCalls = int(line[13])
+        totalNightCalls = float(line[13])
         totalNightCharge = float(line[14])
         totalInternationalMinutes = float(line[15])
-        totalInternationalCalls = int(line[16])
+        totalInternationalCalls = float(line[16])
         totalInternationalCharge = float(line[17])
-        customerServiceCalls = int(line[18])
+        customerServiceCalls = float(line[18])
         churn = line[19]        
 	
 	obj = Row(
@@ -65,17 +60,29 @@ def makeSqlRow(line):
                totalInternationalMinutes =  totalInternationalMinutes,
                totalInternationalCalls =  totalInternationalCalls,
                totalInternationalCharge =  totalInternationalCharge,
-               customerService =  customerServiceCalls,
+               customerServiceCalls =  customerServiceCalls,
                churn  =  churn
 	)
         return obj
 
-conf = ( SparkConf().setAppName('Churn Rate Data Processing2') )
-sc = SparkContext(conf = conf)
+binaryMap = { 'Yes' : 1.0, 'No' : 0.0, 'True' : 1.0, 'False' : 0.0  }
+state0Set = ['CT','MA','ME','NH','NJ','NY','PA','RI','VT'] # NorthEast
+state1Set = ['IA','IL','IN','KS','MI','MN','MO','ND','NE','OH','SD','WI'] #MidWest
+state2Set = ['AL','AR','DC','DE','FL','GA','KY','LA','MD','MS','NC','OK','SC','TN','TX','VA','WV'] #South
+state3Set = ['AK','AZ','CA','CO','HI','ID','MT','NM','NV','OR','UT','WA','WY'] #West
 
-# sc.stop()
+stateMap = { 0.0 : state0Set, 1.0: state1Set, 2.0: state2Set, 3.0: state3Set }
+toRegion = UserDefinedFunction(getRegionFromState, DoubleType() )
+toNum = UserDefinedFunction( lambda k: binaryMap[k], DoubleType() )
 
 path = '../resources/churn-bigml-80.txt'
+
+SPARK_MASTER='local'
+SPARK_APP_NAME='Churn Model'
+conf = SparkConf().setMaster(SPARK_MASTER).setAppName(SPARK_APP_NAME).set( 'spark.driver.allowMultipleContexts' , "true")
+
+sc = SparkContext.getOrCreate(conf = conf) 
+
 rdd1 = sc.textFile(path)
 
 rddWithOutHeader = ( rdd1
@@ -85,7 +92,6 @@ rddWithOutHeader = ( rdd1
                    )
 
 #rddWithOutHeader.filter( lambda obj: obj.state == "NY" ).foreach(f)
-
 
 def getIterableLength(x,y):
 	lenOfGroup = len( y )
@@ -123,10 +129,10 @@ df = sqlContext.createDataFrame(rddWithOutHeader)
 
 df2  = ( 
 	df
-	.select( "internationalPlan", 
+	.select(  "churn",
+		  "internationalPlan", 
 		  "voiceMailPlan", 
 		  "state", 
-	          "churn",
                   "acctLength", 
                   "numOfVmailMessages", 
                   "totalDayMinutes",
@@ -138,9 +144,35 @@ df2  = (
 		  "totalInternationalCalls")
          .withColumn('internationalPlan', toNum( df['internationalPlan'] ) )
          .withColumn('voiceMailPlan', toNum( df['voiceMailPlan'] ) )
-         .withColumn('state', toState( df['state'] ) )
+         .withColumn('region', toRegion( df['state'] ) )
          .withColumn('churn', toNum( df['churn'] ) )
+	 .drop('state')
 	)
-	             
+df2.printSchema()
 df2.show(5)
-#sc.stop()
+
+
+rddLabeledPoints = labelData(df2)
+#rddLabeledPoints.foreach(f)
+
+trainData,testData = rddLabeledPoints.randomSplit([0.80,0.20])
+
+model = DecisionTree.trainClassifier(trainData,numClasses=2,maxDepth=2,categoricalFeaturesInfo={0:2,1:2,11:4}, impurity = 'gini', maxBins = 32)
+
+from pyspark.mllib.evaluation import MulticlassMetrics
+
+def getPredictionsLabels(model, testData):
+    predictions = model.predict(testData.map(lambda r: r.features))
+    return predictions.zip(testData.map(lambda r: r.label))
+
+def printMetrics(predictions_and_labels):
+    metrics = MulticlassMetrics(predictions_and_labels)
+    print 'Precision of True ', metrics.precision(1)
+    print 'Precision of False', metrics.precision(0)
+    print 'Recall of True    ', metrics.recall(1)
+    print 'Recall of False   ', metrics.recall(0)
+    print 'F-1 Score         ', metrics.fMeasure()
+    print 'Confusion Matrix\n', metrics.confusionMatrix().toArray()
+
+predictionsAndLabels = getPredictionsLabels(model, testData)
+printMetrics(predictionsAndLabels)
